8a98c360e43d29beaa106b7a63b046f96c1f6e83
arod571431 <nick930212@gmail.com>
2025-03-24 02:55:04 +0800

    Fix Group HW

--- hw1/programming_project/Ch3/Unixshell.c
+++ hw1/programming_project/Ch3/Unixshell.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -96,7 +97,12 @@ void parse_execute(char *input) {
     add_to_history(input);
     char *args[MAX_ARGS];
     char *token;
-    int i = 0, j = 0;
+    int i = 0, j = 0, pipe_flag = 0;
+    char *input_file = NULL, *output_file = NULL;
+
+    int saved_stdin = dup(STDIN_FILENO);   // Save original stdin
+    int saved_stdout = dup(STDOUT_FILENO); // Save original stdout
+
     while (input[j] != '\0') {
         while (input[j] == ' ') {
             j++;
@@ -116,14 +122,75 @@ void parse_execute(char *input) {
             args[i++] = token;
             continue;
         }
+        if (input[j] == '>') {
+            j++;
+            while (input[j] == ' ') {
+                j++;
+            }
+            output_file = &input[j];
+            while (input[j] != ' ' && input[j] != '\0') {
+                j++;
+            }
+            if (input[j] != '\0') {
+                input[j++] = '\0';
+            }
+            int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) {
+                perror("open");
+                dup2(saved_stdin, STDIN_FILENO);   // Restore stdin
+                dup2(saved_stdout, STDOUT_FILENO); // Restore stdout
+                close(saved_stdin);
+                close(saved_stdout);
+                return;
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            continue;
+        }
+        if (input[j] == '<') {
+            j++;
+            while (input[j] == ' ') {
+                j++;
+            }
+            input_file = &input[j];
+            while (input[j] != ' ' && input[j] != '\0') {
+                j++;
+            }
+            if (input[j] != '\0') {
+                input[j++] = '\0';
+            }
+            int fd = open(input_file, O_RDONLY);
+            if (fd < 0) {
+                perror("open");
+                dup2(saved_stdin, STDIN_FILENO);   // Restore stdin
+                dup2(saved_stdout, STDOUT_FILENO); // Restore stdout
+                close(saved_stdin);
+                close(saved_stdout);
+                return;
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+            continue;
+        }
         token = &input[j];
+        pipe_flag = 0;
         while (input[j] != ' ' && input[j] != '\0') {
+            if (input[j] == '|') {
+                pipe_flag = 1;
+                break;
+            }
             j++;
         }
         if (input[j] != '\0') {
             input[j++] = '\0';
         }
         args[i++] = token;
+        if (pipe_flag || input[j] == '|') {
+            if (input[j] == '|') {
+                j++;
+            }
+            args[i++] = "|";
+        }
     }
     args[i] = NULL;
 
@@ -137,7 +204,19 @@ void parse_execute(char *input) {
         print_history();
         return;
     }
+
+    // printf("Arguments:\n");
+    // for (int k = 0; args[k] != NULL; k++) {
+    //     printf("args[%d]: %s\n", k, args[k]);
+    // }
+
     executeCommandEntry(args, NULL);
+
+    // Restore original stdin and stdout
+    dup2(saved_stdin, STDIN_FILENO);
+    dup2(saved_stdout, STDOUT_FILENO);
+    close(saved_stdin);
+    close(saved_stdout);
 }
 
 int main() {
